dee0fe1935d93c5dc441e6dd8d9ed8b164951c20
petercondoleon <petercondoleon@gmail.com>
2017-09-14 13:16:15 +1000

    nav bar php fix

--- php/universalNav.php
+++ php/universalNav.php
@@ -1,1 +1,10 @@
-echo "<h1>HELLO</h1>"
+<?php
+echo '<h1>Framing Queensland</h1>';
+echo '<ul>\
+        <li><a href="../index.html">Home</a></li>\
+        <li><a href="game.html">Play</a></li>\
+        <li><a href="account.html">Profile</a></li>\
+        <li><a href="leaderboard.html">Leaderboard</a></li>\
+        <li><a href="skydesign.html">About</a></li>\
+      </ul>';
+?>
