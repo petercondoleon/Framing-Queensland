bada4cad8f27d8f49e4d0641b8aa557a477df359
Captagear <31152230+Captagear@users.noreply.github.com>
2017-09-15 15:10:23 +1000

    renamed slq_ajax_call and reworked!
    added todo section to JS. wiki will go up tonight for ease reading for some of the later JS code

--- js/javascript.js
+++ js/javascript.js
@@ -2,7 +2,8 @@
  * Executes when the document is ready.
  */
 $(document).ready(function() {
-  setupUniversalNav();
+    homepageImagesSetup();
+    setupUniversalNav();
 });
 
 /*
@@ -20,3 +21,45 @@ $(document).ready(function() {
       </ul>'
     );
  }
+
+/*
+* Sets up homepage with images.
+* @param {Object[]} an array of objects containing images
+*/
+function homepageImagesSetup(jsonImages) {
+    /*
+    TODO:
+        - call images from slq using:
+            loadSLQImages(count, exclusionData)
+                WHERE count = amount of images 
+                AND exclusionData is "[0]"
+            access image URL like this "jsonImages[i].image"
+        - use helper method to insert images into list:
+            insertImage(image, htmlDOMPos)
+                WHERE image is the source (URL)
+                AND htmlDOMPOS is the location you want 
+                to insert img tag.
+        I suggest something like this;
+            <div>
+                <ul>
+                    <li>+IMAGE+</li>
+                    <li>+IMAGE+</li>
+                </ul>
+            </div>
+        for easy animation later on, unless you are comfortable 
+        doing it another way :)
+            
+            
+    */
+}
+
+/*
+* loads an image at a give html dom pos
+* @param image source
+* @param pos in the DOM that the img will be inserted to
+*/
+function insertImage(image, htmlDOMPos) {
+    $(htmlDOMPos).append(
+    '<img src='+'"'+image+'"'+'/>'
+    );
+}
