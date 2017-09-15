fa40e1b154d9beb417b0200b3e3442f2cb94f6b5
Peter Condoleon <petercondoleon@gmail.com>
2017-09-15 20:35:35 +1000

    Removed PHP code from game.html

--- php/universalNav.php
+++ php/universalNav.php
@@ -1,10 +1,12 @@
 <?php
 echo '<h1>Framing Queensland</h1>';
-echo '<ul>\
-        <li><a href="../index.html">Home</a></li>\
-        <li><a href="game.html">Play</a></li>\
-        <li><a href="account.html">Profile</a></li>\
-        <li><a href="leaderboard.html">Leaderboard</a></li>\
-        <li><a href="skydesign.html">About</a></li>\
+echo '<ul>
+        <li><a href="../index.html">Home</a></li>
+        <li><a href="game.html">Play</a></li>
+        <li><a href="account.html">Profile</a></li>
+        <li><a href="leaderboard.html">Leaderboard</a></li>
+        <li><a href="skydesign.html">About</a></li>
       </ul>';
+
+include '../html/skydesign.html';
 ?>
