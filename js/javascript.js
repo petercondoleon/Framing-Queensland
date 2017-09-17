e608a433cbe948ba6b0ec760b8e1bbc4963b9771
Peter Condoleon <petercondoleon@gmail.com>
2017-09-17 17:27:00 +1000

    Moved navigationBar implementation from JavaScript to PHP

--- js/javascript.js
+++ js/javascript.js
@@ -3,24 +3,7 @@
  */
 $(document).ready(function() {
     homepageImagesSetup();
-    setupUniversalNav();
 });
-
-/*
- * Sets the contents of the navigation bar.
- */
- function setupUniversalNav() {
-    $("#universalNav").html(
-      '<h1>Framing Queensland</h1>\
-      <ul>\
-        <li><a href="../index.html">Home</a></li>\
-        <li><a href="game.html">Play</a></li>\
-        <li><a href="account.html">Profile</a></li>\
-        <li><a href="leaderboard.html">Leaderboard</a></li>\
-        <li><a href="skydesign.html">About</a></li>\
-      </ul>'
-    );
- }
 
 /*
 * Sets up homepage with images.
@@ -31,13 +14,13 @@ function homepageImagesSetup(jsonImages) {
     TODO:
         - call images from slq using:
             loadSLQImages(count, exclusionData)
-                WHERE count = amount of images 
+                WHERE count = amount of images
                 AND exclusionData is "[0]"
             access image URL like this "jsonImages[i].image"
         - use helper method to insert images into list:
             insertImage(image, htmlDOMPos)
                 WHERE image is the source (URL)
-                AND htmlDOMPOS is the location you want 
+                AND htmlDOMPOS is the location you want
                 to insert img tag.
         I suggest something like this;
             <div>
@@ -46,10 +29,10 @@ function homepageImagesSetup(jsonImages) {
                     <li>+IMAGE+</li>
                 </ul>
             </div>
-        for easy animation later on, unless you are comfortable 
+        for easy animation later on, unless you are comfortable
         doing it another way :)
-            
-            
+
+
     */
 }
 
