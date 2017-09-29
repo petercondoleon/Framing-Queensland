aa2622176ff1b09f3f1bdf5be84b6b39dfe52331
Peter Condoleon <petercondoleon@gmail.com>
2017-09-29 20:18:13 +1000

    Updated nav bar
    Added icons to nav bar items and also added a font to the website

--- php/universalNav.php
+++ php/universalNav.php
@@ -1,12 +1,43 @@
-<?php
-echo '<nav class="universalNav">
-        <h1>Framing Queensland</h1>
-        <ul>
-          <li><a href="/index.php">Home</a></li>
-          <li><a href="/php/game.php">Play</a></li>
-          <li><a href="/html/account.html">Profile</a></li>
-          <li><a href="/html/leaderboard.html">Leaderboard</a></li>
-          <li><a href="/html/skydesign.html">About</a></li>
-        </ul>
-      </nav>'
-?>
+<head>
+    <link rel="stylesheet" type="text/css" media="screen" href="/css/universalNav.css"/>
+</head>
+
+<nav class="universalNav">
+    <h1>Framing Queensland</h1>
+    <ul>
+        <li>
+            <a href="/index.php">
+                <img src="/svg/home.svg" alt="home icon">
+                Home
+            </a>
+        </li>
+
+        <li>
+            <a href="/php/game.php">
+                <img src="/svg/play.svg" alt="play icon">
+                Play
+            </a>
+        </li>
+
+        <li>
+            <a href="/html/account.html">
+                <img src="/svg/account.svg" alt="play icon">
+                Account
+            </a>
+        </li>
+
+        <li>
+            <a href="/html/leaderboard.html">
+                <img src="/svg/leaderboard.svg" alt="play icon">
+                Leaderboard
+            </a>
+        </li>
+
+        <li>
+            <a href="/html/skydesign.html">
+                <img src="/svg/about.svg" alt="play icon">
+                About
+            </a>
+        </li>
+    </ul>
+</nav>
