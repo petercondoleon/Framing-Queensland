701e9335e97b0ea81a62c95cc420558e8da78348
Peter Condoleon <petercondoleon@gmail.com>
2017-10-09 12:07:12 +1000

    Changed the PHP and HTML file structure to be more modular

---
+++ php/account.php
@@ -0,0 +1,7 @@
+<?php
+    // Add the navigation bar to the top of the page
+    include $_SERVER['DOCUMENT_ROOT'].'/html/navigation.html';
+
+    // Include the html of the account page
+    include $_SERVER['DOCUMENT_ROOT'].'/html/account.html';
+?>
---
+++ php/leaderboard.php
@@ -0,0 +1,7 @@
+<?php
+    // Add the navigation bar to the top of the page
+    include $_SERVER['DOCUMENT_ROOT'].'/html/navigation.html';
+
+    // Include the html of the leaderboard page
+    include $_SERVER['DOCUMENT_ROOT'].'/html/leaderboard.html';
+?>
