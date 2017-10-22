9fbd573bc56b3cf13c2af1b75b5ee5085d3470a5
Captagear <31152230+Captagear@users.noreply.github.com>
2017-10-22 21:46:27 +1000

    api key removed for incase of accidental usage

--- php/resource.php
+++ php/resource.php
@@ -2,8 +2,8 @@
     $data_id = '9913b881-d76d-43f5-acd6-3541a130853d';
     // NOTE: on line 5 is the API key, swap the comment to line 6 for testing
     // NOTE: PLEASE BE AWARE OF USAGE
-    $api_key = 'e2c49cf4bd8f4280a237edc9b86f91b7';
-    // $api_key = 'NaNaNaNaNa';
+    //$api_key = 'e2c49cf4bd8f4280a237edc9b86f91b7';
+    $api_key = 'NaNaNaNaNa';
 ?>
 <script type="text/javascript">
     "use strict";
